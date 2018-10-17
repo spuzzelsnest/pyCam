8e751998cc819f5cc992303be9b97de53e31f486
spuzzelsnest <j.mpdesmet@gmail.com>
2018-10-18 00:04:32 +0200

    split prep and app

--- homeSec.sh
+++ homeSec.sh
@@ -1,35 +1,13 @@
 #!/bin/bash
-user=$(whoami)
-groups=( mail video motion )
-programs=( ssmtp fswebcam zip mail motion )
-dump='pix'
-zips='sendFiles'
-date=$(date +"%Y-%m-%d_%H%M")
-email=j.mpdesmet@gmail.com
 
+user=$(whoami)
+dump='/home/'$user'/Pictures/homeSec'
+zips='/home/'$user'/Pictures/sendFiles'
+date=$(date +"%Y-%m-%d_%H%M")
+echo 'what email you want to send it too?'
+read email
 mkdir -p $dump
 mkdir -p $zips
-
-
-echo hi $user test, test
-echo --------------------------
-for i in "${groups[@]}"; do
-    if getent group $i  | grep &>/dev/null "\b${user}\b"; then
-        echo Member of group $i
-    else
-        echo No Member of group $i
-	sudo adduser $user $i
-    fi
-done
-
-
-sudo apt-get update
-
-for program in "${programs[@]}"; do
-    if ! command -v "$program" > /dev/null 2>&1; then
-        sudo apt-get install "$program" -y
-    fi
-done
 
 echo ---------------
 echo taking picture
---
+++ preps.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+user=$(whoami)
+groups=( mail video motion )
+programs=( ssmtp fswebcam zip mail motion )
+
+echo hi $user test, test
+echo --------------------------
+sudo apt-get update
+for program in "${programs[@]}"; do
+    if ! command -v "$program" > /dev/null 2>&1; then
+        sudo apt-get install "$program" -y
+    fi
+done
+
+echo Checking Groups
+echo ---------------
+for i in "${groups[@]}"; do
+    if getent group $i  | grep &>/dev/null "\b${user}\b"; then
+        echo Member of group $i
+    else
+        echo No Member of group $i
+        sudo adduser $user $i
+    fi
+done
+
+echo done prepping
