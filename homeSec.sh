43a22fcd8f0cb9b75bce5e9a7ecc7d165fed03d9
spuzzelsnest <j.mpdesmet@gmail.com>
2021-07-23 17:41:41 +0100

    reverted to old version info

--- homeSec.sh
+++ homeSec.sh
@@ -1,30 +1,29 @@
 #!/bin/bash
 
-user=$(whoami)
-dump='/home/'$user'/Pictures/homeSec'
-zips='/home/'$user'/Pictures/sendFiles'
+dump='/home/ggm/Pictures/homeSec'
+zips='/home/ggm/Pictures/sendFiles'
 date=$(date +"%Y-%m-%d_%H%M")
-email= <! email address !>
+email='j.mpdesmet@protonmail.com'
 mkdir -p $dump
 mkdir -p $zips
-
 
 echo '----------------'
 echo '|Taking picture|'
 echo '----------------'
+
 fswebcam -r 1280x720 --no-banner $dump/$date.jpg
 
 sleep 1m
 
 echo '-------------------------------------'
-echo '|Zipping files and removing org File|'
+echo '|Zipping files and removing orf File|'
 echo '-------------------------------------'
 cd $dump
 zip -r $zips/homeSec_$date.zip *
 rm $dump/*
 
-echo '---------------------------------------------------------'
+echo '--------------------------------------------------------'
 echo '|sending' $zips/homeSec_$date.zip to $email
 echo '---------------------------------------------------------'
 
-echo "neuze neuze" | mutt -s "Motion Detected" -a $zips/homeSec_$date.zip -- $email
+echo "neuze neuze" | mail -s "Motion Detected" $email -A $zips/homeSec_$date.zip
