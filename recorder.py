96f949556b95397614033a6531f662245a7c7a60
spuzzelsnest <j.mpdesmet@gmail.com>
2023-03-23 13:48:30 +0000

    smuckup

--- recorder.py
+++ recorder.py
@@ -1,41 +1,52 @@
 #!/usr/bin/python3
+# REQUIREMENTS
+# pip3 install psutil picamera
 
-import sys
+import os, sys, socket, errno
 import psutil
+import datetime
+
 from picamera import PiCamera
 from time import sleep
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-print(sys.argv[1:])
-
-def checkProcess(processName):
-
-    #Iterate over the all the running process
-    for proc in psutil.process_iter():
-        try:
-            # Check if process name contains the given name string.
-            if processName.lower() in proc.name().lower():
-                proc.kill()
-                return True
-        except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
-            pass
-    return False;
-
+#VARS
+date = datetime.datetime.now().strftime('%m-%d-%Y_%H.%M.%S')
+recFile = '/home/hikvision/FTP/recording' + date + '.h264'
+secret = str(sys.argv[1:])
 
 def recCam():
-    rec_file="/home/hikvision/FTP/recording.h264"
-
     camera = PiCamera()
     camera.rotation = 180
     camera.resolution = (1024, 768)
-    camera.annotate_text = str(sys.argv[1:])
-    camera.start_recording(rec_file)
+    camera.start_preview()
+    camera.annotate_text = secret
+    camera.start_recording(recFile)
     sleep(5)
     camera.stop_recording()
-    camera.close()
-
-if checkProcess('motion'):
-    print('Yes a motion process was running and we tried to stop it')
-    recCam()
+def testPort(port):
+    try:
+        s.bind(('127.0.0.1', port))
+    except socket.error as e:
+        if e.errno == errno.EADDRINUSE:
+            print('Port is already in use, we need to close it')
+            os.system('sudo /bin/systemctl stop motion.service')
+            sleep(2)
+            print('The service should be stopped now')
+            return False
+        else:
+            # something else raised the socket.error exception
+            print(e)
+    s.close()
+if testPort(8081):
+    print('Yes a motion process is running ')
+    testPort(8081)
 else:
-    print('No motion process was running')
+    print('Starten Suspicious Recording', secret)
     recCam()
+    print('... Recording Done ...')
+    sleep(2)
+    print('Restarting Live View')
+    os.system('sudo motion -b')
+    sleep(2)
+    print('Live View Restarted')
