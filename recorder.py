0980440459579b6fdd903448e2539867b25e9c5d
spuzzelsnest <j.mpdemset@protonmail.com>
2024-05-27 19:58:16 +0200

    updated recorder

--- recorder.py
+++ recorder.py
@@ -4,6 +4,7 @@ import sys, logging, socket, datetime, time
 
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
+from picamera2.outputs import FileOutput
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
@@ -19,7 +20,7 @@ recFile = '/tmp/FTP/recordings/'+ team +'_'+ date +'.h264'
 
 encoderRecorder = H264Encoder(10000000)
 
-camera.start_encoder(encoder_recorder, FileOutput(recFile))
+camera.start_encoder(encoderRecorder, FileOutput(recFile))
 time.sleep(RECORDING_DURATION)
 camera.stop_encoder(encoderRecorder)
 logging.info("Recording Stopped")
