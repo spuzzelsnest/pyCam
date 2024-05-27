651e9b13b585b744eac5f70225c79373b94c508f
spuzzelsnest <j.mpdemset@protonmail.com>
2024-05-27 21:16:16 +0200

    recorder updated with text

--- recorder.py
+++ recorder.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-import sys, logging, socket, datetime, time
+import sys, logging, socket, datetime, time, subprocess
 
 from picamera2 import Picamera2
 from picamera2.encoders import H264Encoder
@@ -17,6 +17,7 @@ date = datetime.datetime.now().strftime('%m-%d-%Y_%H.%M.%S')
 secret = str(sys.argv[1])
 team = str(sys.argv[2])
 recFile = '/tmp/FTP/recordings/'+ team +'_'+ date +'.h264'
+outputFile = '/FTP/recordings/'+ team +'_'+ date +'.h264'
 
 encoderRecorder = H264Encoder(10000000)
 
@@ -24,3 +25,19 @@ camera.start_recording(encoderRecorder, FileOutput(recFile))
 time.sleep(RECORDING_DURATION)
 camera.stop_recording()
 logging.info("Recording Stopped")
+
+# Add text to Video
+ffmpeg_command = [
+
+    'ffmpeg',
+    '-i', rec_file,
+    '-vf', f"drawtext=text='{team} - {secret}':fontcolor=white:fontsize=24:x=10:y=10",
+    '-codec:a', 'copy',
+    outputFile
+]
+
+try:
+    subprocess.run(ffmpeg_command, check=True)
+    logging.info(f"Text overlay added to video: {output_file}")
+except subprocess.CalledProcessError as e:
+    logging.error(f"Failed to add text overlay: {e}")
