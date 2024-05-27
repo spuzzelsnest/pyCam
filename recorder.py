080f398e0cebe669be49abab8919f75e27b68851
spuzzelsnest <j.mpdemset@protonmail.com>
2024-05-27 19:35:59 +0200

    return of the recorder.py

---
+++ recorder.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+import sys, logging, socket, datetime, time
+
+from picamera2 import Picamera2
+from picamera2.encoders import H264Encoder
+
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+#VARS
+
+camera = Picamera2()
+RECORDING_DURATION = 10
+
+date = datetime.datetime.now().strftime('%m-%d-%Y_%H.%M.%S')
+secret = str(sys.argv[1])
+team = str(sys.argv[2])
+recFile = '/tmp/FTP/recordings/'+ team +'_'+ date +'.h264'
+
+encoderRecorder = H264Encoder(10000000)
+
+camera.start_encoder(encoder_recorder, FileOutput(recFile))
+time.sleep(RECORDING_DURATION)
+camera.stop_encoder(encoderRecorder)
+logging.info("Recording Stopped")
